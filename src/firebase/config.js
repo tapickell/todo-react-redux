38621449a291b9b927013a5f34b9304adea09c9f
Todd Pickell <tapickell@gmail.com>
2018-01-14 22:46:05 -0600

    getting going with firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,13 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBsVVpEDrlNPEmshLcmOuE0FxhjPn0AqMg',
-  authDomain: 'todo-react-redux.firebaseapp.com',
-  databaseURL: 'https://todo-react-redux.firebaseio.com',
-  storageBucket: 'firebase-todo-react-redux.appspot.com'
+//   apiKey: 'AIzaSyBsVVpEDrlNPEmshLcmOuE0FxhjPn0AqMg',
+//   authDomain: 'todo-react-redux.firebaseapp.com',
+//   databaseURL: 'https://todo-react-redux.firebaseio.com',
+//   storageBucket: 'firebase-todo-react-redux.appspot.com'
+// };
+  apiKey: "AIzaSyA9gyoEzIZtuXuU-7kjsutMDzgFMQ-ggg8",
+  authDomain: "resplendent-inferno-6555.firebaseapp.com",
+  databaseURL: "https://resplendent-inferno-6555.firebaseio.com",
+  projectId: "resplendent-inferno-6555",
+  storageBucket: "resplendent-inferno-6555.appspot.com",
+  messagingSenderId: "334072765336"
 };
